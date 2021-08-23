b3fbd4c21ba92fbebd1537c61c973af10dd4a979
Doug Jones <DSJR1284@GMAIL.COM>
2021-08-23 16:12:29 -0400

    add user and answer model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :answers
+  resources :users
   resources :languages
   resources :questions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/migrate/20210819215932_create_questions.rb
+++ db/migrate/20210819215932_create_questions.rb
@@ -1,7 +1,10 @@
 class CreateQuestions < ActiveRecord::Migration[6.1]
   def change
     create_table :questions do |t|
-
+      t.string :title
+      t.text :question
+      t.text :answer
+      t.integer :language_id
       t.timestamps
     end
   end
