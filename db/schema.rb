0abaa1fbe849b77edc23dac66bc675b96c28ad3f
Doug Jones <DSJR1284@GMAIL.COM>
2021-08-23 19:03:42 -0400

    add user_id to answers table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_23_201208) do
+ActiveRecord::Schema.define(version: 2021_08_23_225625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_08_23_201208) do
     t.integer "question_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "languages", force: :cascade do |t|
